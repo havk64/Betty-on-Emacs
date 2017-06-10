a29b1b943dd316d4e14635d510afc57bc6fc7d79
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-10 20:44:17 -0300

    Fix typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Uses GNU Stow to install the files in the proper folder allowing easy
 # install(stow) and uninstall(unstow) of symlinks to this source controlled repo
 
-targer=~/
+target=~/
 
 # Installs GNU Stow if not installed(just 200kb)
 which stow || sudo apt-get -y install stow
