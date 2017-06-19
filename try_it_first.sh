23f74eb6a0687c3d4ba4229cea888d66408b97ae
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-19 03:02:19 -0300

    Add explanation/comments

--- try_it_first.sh
+++ try_it_first.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# This script run Emacs using my .emacs config file (-l) instead of
+# the config file of your ~/ home directory, your personal configuration.
+# Thus you can try this configuration without affect your current settings.
 
 emacs -q -l .emacs
