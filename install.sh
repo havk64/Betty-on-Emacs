a49f084797e08f1ba3d6739605478646e4a05108
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-20 02:50:46 -0300

    Meaningful variable name

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 # install(stow) and uninstall(unstow) of symlinks to this source controlled repo
 # shellcheck disable=SC2015
 
-# The variable script_dir allow this script to be called from other directories
+# The variable stow_dir allow this script to be called from other directories
 target=~/
-script_dir="$(pwd)/$(dirname $BASH_SOURCE)"
+stow_dir="$(pwd)/$(dirname $BASH_SOURCE)"
 
 # Check for Operating system to install Stow
 install_stow() {
@@ -28,7 +28,7 @@ which stow && echo "Stow found at $(which stow)" || install_stow
 # The call for Stow uses the script's directory as "Stow directory" and the home
 # directory as the "Target". For more info take a look at "Stow terminology"
 # at: https://www.gnu.org/software/stow/manual/html_node/Terminology.html
-stow -d "$script_dir" -t $target -S betty -vvv
+stow -d "$stow_dir" -t $target -S betty -vvv
 
 echo "All set!"
 exit
