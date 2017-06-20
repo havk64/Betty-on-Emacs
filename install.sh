8c42c63b3af3dc164003fa7b849f2635f1fd6fc0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-20 02:14:46 -0300

    Add more comments/explanation

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # install(stow) and uninstall(unstow) of symlinks to this source controlled repo
 # shellcheck disable=SC2015
 
+# The variable script_dir allow this script to be called from other directories
 target=~/
 script_dir="$(pwd)/$(dirname $BASH_SOURCE)"
 
@@ -24,6 +25,9 @@ install_stow() {
 # Installs GNU Stow if not installed(just 200kb)
 which stow && echo "Stow found at $(which stow)" || install_stow
 
+# The call for Stow uses the script's directory as "Stow directory" and the home
+# directory as the "Target". For more info take a look at "Stow terminology"
+# at: https://www.gnu.org/software/stow/manual/html_node/Terminology.html
 stow -d "$script_dir" -t $target -S betty -vvv
 
 echo "All set!"
