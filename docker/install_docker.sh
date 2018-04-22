7ae259c8bfa2d269687fedad48b5f7f4a8a9772d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-04-22 10:38:41 -0700

    Add docker directory with conf and install files for emacs docker image

---
+++ docker/install_docker.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Install betty-style as a C syntax checker on the fly, for Emacs
+
+cp .emacs ~/
+[ ! -d ~/.emacs.d ] && mkdir -p ~/.emacs.d
+cp -r ../betty/.emacs.d/private ~/.emacs.d
