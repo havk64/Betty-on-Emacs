7f57ceee2490b15a9c274ebcca7724c53086ca7b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-04-22 11:06:49 -0700

    Add debugging flag

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Install betty-style as a C syntax checker on the fly, for Emacs
 
+set -ex
 cp .emacs ~/
 [ ! -d ~/.emacs.d ] && mkdir -p ~/.emacs.d
 cp -r ../betty/.emacs.d/private ~/.emacs.d
