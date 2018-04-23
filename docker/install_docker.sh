cdc66e6f527cf11edef63731b2bd418acc41f7a6
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-04-22 17:39:19 -0700

    Update install script to install emacs dark theme

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -3,5 +3,6 @@
 
 set -ex
 cp .emacs ~/
-[ ! -d ~/.emacs.d ] && mkdir -p ~/.emacs.d
+[ ! -d ~/.emacs.d ] && mkdir -p ~/.emacs.d/themes
 cp -r ../betty/.emacs.d/private ~/.emacs.d
+cp ../betty/.emacs.d/themes/emacs/dracula-theme.el ~/.emacs.d/themes
